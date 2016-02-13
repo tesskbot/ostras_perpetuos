bc8764dd3e3d4eabca0e14d24e7364a4b6223ca2
Tess <tess.kornfield@gmail.com>
2016-02-13 08:50:27 -0800

    initialize data cleaning

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ostras_perpetuos
+
+## Goal
+Where are oysters in SF? Well, what time is it? See the establishments on a map. Go there. Eat them oysters.
---
+++ develop/2016-02-13-clean-data.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import pandas as pd
+
+
+# In[3]:
+
+df = pd.read_excel('../data/one_dollar_oysters.xlsx')
+
+
+# In[10]:
+
+df.head(n=2)
+
+
+# In[12]:
+
+df.shape
+
+
+# In[ ]:
+
+
+
